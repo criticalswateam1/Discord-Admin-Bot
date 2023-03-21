8f5d02ea7d9dface119a1171077f1044ab147fba
criticalswateam1 <caeden.rajoo@outlook.com>
2023-03-21 16:02:09 +0200

    hi

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: `SYSTEMBOTS_${require("path").resolve(__dirname).split("/")[6]}`,
+      script: "index.js",
+      cron_restart: "0 1 * * *",
+    },
+  ],
+};
